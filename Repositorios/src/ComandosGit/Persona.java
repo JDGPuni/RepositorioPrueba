3a8fcce27057e25337e59023545d7168df94b57c
JDGPuni <juand.gutierrezp@uqvirtual.edu.co>
2023-03-30 21:11:33 -0500

    commit#2 se creó el método constructor y se le agregaron los atributos por parámetros y se crearon los this

--- Repositorios/src/ComandosGit/Persona.java
+++ Repositorios/src/ComandosGit/Persona.java
@@ -4,6 +4,11 @@ public class Persona {
 
 	private String nombre;
 	private String correo;
+	
+	public Persona(String nombre, String correo) {
+		this.nombre=nombre;
+		this.correo=correo;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
